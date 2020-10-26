c03f89614886783c84189509d7d30dadf5962d3c
Larizza <larissa-dannbacherr@web.de>
2020-10-26 15:36:03 +0100

    problem

--- src/Screens/ItemScreen.js
+++ src/Screens/ItemScreen.js
@@ -4,49 +4,61 @@ import * as SQLite from 'expo-sqlite';
 
 import Style from '../Style/Style';
 import ItemList from '../Components/ItemList';
+import SearchInput from '../Components/SearchInput';
+
 
 // Datenbankverbindung
 const database = SQLite.openDatabase('eft_twelve.db');
 
 export default class ExchangeObjectsScreen extends Component {
-  state = {items: []};
+  state = { items: [], input: '' };
 
   _getData = async (myCallback) => {
-    let innerJoinString = 'SELECT Name FROM table_armbands UNION SELECT Name FROM table_backpacks UNION SELECT Name FROM table_provisions UNION SELECT Name FROM table_headsets UNION SELECT Name FROM table_eyewears UNION SELECT Name FROM table_containers UNION SELECT Name FROM table_secure_containers ORDER BY Name ASC';
+    let innerJoinString = 'SELECT name FROM table_armbands UNION SELECT name FROM table_backpacks UNION SELECT name FROM table_provisions UNION SELECT name FROM table_headsets UNION SELECT name FROM table_eyewears UNION SELECT name FROM table_containers UNION SELECT name FROM table_secure_containers ORDER BY name ASC';
 
     database.transaction((tx) => {
-      tx.executeSql(innerJoinString, [], 
-      (tx,results) => {
-        myCallback(results.rows._array);
-      });
+      tx.executeSql(innerJoinString, [],
+        (tx, results) => {
+          myCallback(results.rows._array);
+        });
     });
   }
 
   componentDidMount() {
     this._getData((rows) => {
-      this.setState({items: rows});
+      this.setState({ items: rows });
     });
-  } 
+  }
 
   render() {
     const navigation = this.props.navigation
     let styles = Style(Dimensions.get('window').width);
-    let text = 'Search'; 
+
 
     return (
       <View style={styles.container}>
         <ImageBackground source={styles.backgroundImage} style={styles.image}>
           <View style={styles.header}>
-            <TextInput style={styles.textInput} /> 
+            <TextInput
+              style={styles.textInput}
+              placeholder='Search for Item'
+              onChangeText={text => this.setState({input: text})}
+            
+            />
+            {console.log(this.state.input)}
           </View>
-          <View style={styles.content}>  
-            <FlatList 
+          <View style={styles.content}>
+            <FlatList
               data={this.state.items}
-              keyExtractor = {items => items.Name} //später ID verwenden als Schlüssel
-              renderItem = {({item}) => (
-                <ItemList item = {item} onPress = {() => navigation.navigate('Detail', { detail: item.Name})} />
+              keyExtractor={items => items.Name} //später ID verwenden als Schlüssel
+              renderItem={({ item }) => ({
+                if(this.item.Name.localeCompare(this.state.input)){
+                  return <ItemList item={item} onPress={() => navigation.navigate('Detail', { detail: item.Name })} />
+                }
+              }
+                
               )}
-              ItemSeparatorComponent = {() => <View style={styles.listSeparator} />}
+              ItemSeparatorComponent={() => <View style={styles.listSeparator} />}
             />
           </View>
           <View style={styles.footer}>
